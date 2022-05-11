cd9b641a4311362d8e5d2812254353cb2cd7d3cf
noaki-k <knk43nok@gmail.com>
2022-05-12 06:37:56 +0900

    add: chapter 3

--- main.tf
+++ main.tf
@@ -2,13 +2,3 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-2022-05-10"
-    key    = "global/s3/terraform.tfstate"
-    region = "ap-northeast-1"
-
-    dynamodb_table = "terraform-up-and-running-locks-2022-05-10"
-    encrypt        = true
-  }
-}
---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-2022-05-10"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "ap-northeast-1"
+
+    dynamodb_table = "terraform-up-and-running-locks-2022-05-10"
+    encrypt        = true
+  }
+}
+
+resource "aws_db_instance" "example" {
+  allocated_storage   = 10
+  identifier_prefix  = "terraform-up-and-running"
+  engine              = "mysql"
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
