c1c77e1e6d57ab9f5e06bd3ecf1693bdaae65978
noaki-k <knk43nok@gmail.com>
2022-05-16 06:15:39 +0900

    add: module

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -1,13 +1,13 @@
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-  default     = 8080
+  default     = 80
 }
 
 variable "alb_name" {
   description = "The name of the ALB"
   type        = string
-  default     = "terraform-asg-example"
+  default     = "lbtest"
 }
 
 variable "instance_security_group_name" {
---
+++ stage/servers/webserver-cluster/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+}
